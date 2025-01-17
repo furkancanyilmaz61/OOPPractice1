669ab63d82be3cf74bc7dd943dd6b606699bfa6c
Furkancan <furkancaanyilmaz@gmail.com>
2025-01-17 15:32:47 +0300

    Update Program.cs

--- Pratik1/Program.cs
+++ Pratik1/Program.cs
@@ -11,7 +11,7 @@ namespace Pratik1
             {
                 Name = "Furkancan",
                 Surname = "Yilmaz",
-                Yas = 24,
+                Age = 24,
                 BirthDate = new DateTime(2000, 12, 22),
             };
 
@@ -19,7 +19,7 @@ namespace Pratik1
             {
                 Name = "Azra",
                 Surname = "Ozmentese",
-                Yas = 21,
+                Age = 21,
                 BirthDate = new DateTime(2003, 05, 27),
             };
 
@@ -28,25 +28,25 @@ namespace Pratik1
             {
                 Name = "Sertan",
                 Surname = "Bozkus",
-                Yas = 33,
+                Age = 33,
                 BirthDate = new DateTime(1990, 3, 15),
             };
             Person teacher2 = new Person
             {
                 Name = "Kerim",
                 Surname = "Cevik",
-                Yas = 28,
+                Age = 28,
                 BirthDate = new DateTime(1995, 8, 8),
             };
 
 
 
             // Nesneleri konsola yazdırma
-            Console.WriteLine($"Student 1: {student.Name} {student.Surname}, Age: {student.Yas}, Birth Date: {student.BirthDate:dd MMMM yyyy}");
-            Console.WriteLine($"Student 2: {student2.Name} {student2.Surname}, Age: {student2.Yas}, Birth Date: {student2.BirthDate:dd MMMM yyyy}");
+            Console.WriteLine($"Student 1: {student.Name} {student.Surname}, Age: {student.Age}, Birth Date: {student.BirthDate:dd MMMM yyyy}");
+            Console.WriteLine($"Student 2: {student2.Name} {student2.Surname}, Age: {student2.Age}, Birth Date: {student2.BirthDate:dd MMMM yyyy}");
 
-            Console.WriteLine($"Teacher 1: {teacher.Name} {teacher.Surname}, Age: {teacher.Yas}, Birth Date: {teacher.BirthDate:dd MMMM yyyy}");
-            Console.WriteLine($"Teacher 2: {teacher2.Name} {teacher2.Surname}, Age: {teacher2.Yas}, Birth Date: {teacher2.BirthDate:dd MMMM yyyy}");
+            Console.WriteLine($"Teacher 1: {teacher.Name} {teacher.Surname}, Age: {teacher.Age}, Birth Date: {teacher.BirthDate:dd MMMM yyyy}");
+            Console.WriteLine($"Teacher 2: {teacher2.Name} {teacher2.Surname}, Age: {teacher2.Age}, Birth Date: {teacher2.BirthDate:dd MMMM yyyy}");
         }
     }
 }
