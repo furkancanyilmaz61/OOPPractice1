c9ba96a09d40cf24c92cfaec9d3cdab5f8d260db
Furkancan <furkancaanyilmaz@gmail.com>
2025-01-17 15:32:58 +0300

    Update Person.cs

--- Pratik1/Person.cs
+++ Pratik1/Person.cs
@@ -12,7 +12,7 @@ namespace Pratik1
 
     public string Surname { get; set; }
 
-    public int Yas { get; set; }
+    public int Age { get; set; }
     public DateTime BirthDate { get; set; }
 
     }
